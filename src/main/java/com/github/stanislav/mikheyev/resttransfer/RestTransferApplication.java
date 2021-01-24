cd1687a59156345aa62b918b17cf62ca49a636c3
Stanislav Mikheyev <stanislav.mikheyev@gmail.com>
2021-01-24 22:34:09 +0100

    Initial commit

---
+++ src/main/java/com/github/stanislav/mikheyev/resttransfer/RestTransferApplication.java
@@ -0,0 +1,13 @@
+package com.github.stanislav.mikheyev.resttransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTransferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/stanislav/mikheyev/resttransfer/RestTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stanislav.mikheyev.resttransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
