51b7c3bd8d64fa2671bceeaf1a7fe2d2ce7ee9a1
Stanislav Mikheyev <stanislav.mikheyev@gmail.com>
2021-01-24 23:57:10 +0100

    Change AccountNotFoundException to be similar to other exceptions

--- src/main/java/com/github/stanislav/mikheyev/resttransfer/account/AccountNotFoundException.java
+++ src/main/java/com/github/stanislav/mikheyev/resttransfer/account/AccountNotFoundException.java
@@ -3,6 +3,13 @@ package com.github.stanislav.mikheyev.resttransfer.account;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Account can not be found using provided data")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException() {
+        super("Account can not be found using provided data");
+    }
+
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
 }
